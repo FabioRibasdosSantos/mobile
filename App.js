a9e67009ded37715e853f0b0e6598fb2e758b14f
Fabio Ribas dos Santos <fabyo_27@hotmail.com>
2023-05-13 15:26:48 -0300

    alterado

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Routes from '../mobile/src/routes'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
